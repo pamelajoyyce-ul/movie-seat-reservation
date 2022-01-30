e319aa58fe543bd64b3df3b2eff1b5cb62728034
pamelajoyyce <pulacson.halcyondigital@gmail.com>
2022-01-30 13:01:23 +0800

    fix ui for seats

--- src/app/components/seat-map/seat-map.component.ts
+++ src/app/components/seat-map/seat-map.component.ts
@@ -6,6 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seat-map.component.scss']
 })
 export class SeatMapComponent implements OnInit {
+  totalItems = 6;
+  colsPerRow = 2;
+  seatSlots = [
+    {
+      id: 1,
+      seat_no: 1,
+      status: 'available',
+      selected: false
+    },
+    {id: 2,
+      seat_no: 2,
+      status: 'available',
+      selected: false
+    },
+    {id: 3,
+      seat_no: 3,
+      status: 'available',
+      selected: false
+    },
+    {id: 4,
+      seat_no: 4,
+      status: 'unavailable',
+      selected: false
+    },
+    {id: 5,
+      seat_no: 5,
+      status: 'available',
+      selected: false
+    },
+    {id: 6,
+      seat_no: 6,
+      status: 'unavailable',
+      selected: false
+    },
+    {
+      id: 7,
+      seat_no: 7,
+      status: 'available',
+      selected: false
+    },{
+      id: 7,
+      seat_no: 7,
+      status: 'available',
+      selected: false
+    },
+  ]
 
   constructor() { }
 
